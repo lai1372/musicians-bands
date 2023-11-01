14938fe00b5c2da74ce445188f953a243e909087
Laila Case <lai_case@hotmail.com>
2023-11-01 15:05:03 +0000

    add tests for deleting

--- index.test.js
+++ index.test.js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize");
 const { sequelize } = require("./db");
 const { Band, Musician, Song } = require("./index");
 
@@ -48,23 +49,53 @@ describe("Band, Musician, and Song Models", () => {
   test("can update a Musician", async () => {
     // TODO - test updating a musician
     const testMusician = await Musician.create({
-        name: "Shabaka Hutchings",
-        instrument: "Saxaphone",
-      });
-    const updatedMusician = testMusician.update({
-        instrument: "Sax"
-    })
+      name: "Shabaka Hutchings",
+      instrument: "Saxaphone",
+    });
+    const updatedMusician = await testMusician.update({
+      instrument: "Sax",
+    });
 
     expect(testMusician.instrument).toBe("Sax");
   });
 
   test("can delete a Band", async () => {
     // TODO - test deleting a band
-    expect("NO TEST").toBe("EXPECTED VALUE HERE");
+    const testBand = await Band.create({
+      name: "Arctic Monkeys",
+      genre: "Indie Rock",
+    });
+    await Band.destroy({ where: { name: "Arctic Monkeys" } });
+    expect(await Band.findAll()).toEqual([]);
   });
 
   test("can delete a Musician", async () => {
+    const testMusician = await Musician.create({
+      name: "Shabaka Hutchings",
+      instrument: "Saxaphone",
+    });
+    await Musician.destroy({ where: { name: "Shabaka Hutchings" } });
     // TODO - test deleting a musician
-    expect("NO TEST").toBe("EXPECTED VALUE HERE");
+    expect(await Musician.findAll()).toEqual([]);
+  });
+
+  test("can delete a Musician when more than one entry exists", async () => {
+    const testMusician = await Musician.create({
+      name: "Shabaka Hutchings",
+      instrument: "Saxaphone",
+    });
+    const testMusician2 = await Musician.create({
+      name: "Dave Grohl",
+      instrument: "Drums",
+    });
+    await Musician.destroy({ where: { name: "Dave Grohl" } });
+
+    // TODO - test deleting a musician
+    expect(await Musician.findOne({ where: { name: "Dave Grohl" } })).toBe(
+      null
+    );
+    expect(
+      await Musician.findOne({ where: { name: "Shabaka Hutchings" } })
+    ).not.toBe(null);
   });
 });
